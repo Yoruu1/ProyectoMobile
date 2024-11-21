69a9026601d1c9e37c0d2b662ed7c359ea8afb52
Yoruu1 <ax.altamirano@duocuc.cl>
2024-11-20 22:48:04 -0300

    AVANCE SCANER QR

--- src/app/pages/codigoqr/codigoqr.module.ts
+++ src/app/pages/codigoqr/codigoqr.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { CodigoqrPageRoutingModule } from './codigoqr-routing.module';
 
 import { CodigoqrPage } from './codigoqr.page';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { CodigoqrPage } from './codigoqr.page';
     IonicModule,
     CodigoqrPageRoutingModule
   ],
-  declarations: [CodigoqrPage]
+  declarations: [CodigoqrPage, BarcodeScanningModalComponent]
 })
 export class CodigoqrPageModule {}
--- src/app/pages/codigoqr/codigoqr.page.ts
+++ src/app/pages/codigoqr/codigoqr.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner, LensFacing } from '@capacitor-mlkit/barcode-scanning';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
+import { ModalController, ToastController,Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-codigoqr',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CodigoqrPage implements OnInit {
 
-  constructor() { }
+  scanResult = ''
 
-  ngOnInit() {
+  constructor(
+    private modalControler: ModalController,
+    private platform : Platform
+  ) { }
+
+  ngOnInit(): void {
+    if(this.platform.is('capacitor')){
+      BarcodeScanner.isSupported().then();
+      BarcodeScanner.checkPermissions().then();
+      BarcodeScanner.removeAllListeners().then();
+    }
+}
+
+async startScan() {
+  const modal = await this.modalControler.create({
+  component: BarcodeScanningModalComponent,
+  cssClass: 'barcode-scanning-modal',
+  showBackdrop: false,
+  componentProps: {  
+    formats : [],
+    LensFacing : LensFacing.Back,
+
   }
+  });
+
+  await modal.present();
+
+  const {data} = await modal.onWillDismiss();
+  if (data) {
+    this.scanResult = data?.barcode?.displayValue
+  }
+}
+
+
+
+
+
+
 
 }
